c4084b8bd7a6b6e689b5a56aa4b14c95afd27ded
Aaron Penido <aaron.penido@gmail.com>
2018-02-21 15:46:17 -0300

    Add Bishop class

---
+++ src/main/java/com/chessmasters/chessapi/Bishop.java
@@ -0,0 +1,67 @@
+package com.chessmasters.chessapi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bishop extends Piece {
+
+    private List<Square> moves = new ArrayList<>();
+
+    public Bishop(Color color, Square square) {
+        super(color, square);
+    }
+
+    @Override
+    public List<Square> moves() {
+        moves.addAll(leftDiagonal());
+        moves.addAll(rightDiagonal());
+
+        return moves;
+    }
+
+    private List<Square> rightDiagonal() {
+        List<Square> diagonals = new ArrayList<>();
+
+        if(square.getLetter() != 'H') {
+            int numberAhead = square.getNumber() + 1;
+            int numberBehind = square.getNumber() - 1;
+
+            for (int letter = ((int)square.getLetter() + 1); letter <= 72; letter++) {
+                if(square.getNumber() < 8 && numberAhead <= 8) {
+                    diagonals.add(new Square((char)letter, numberAhead));
+                    numberAhead++;
+                }
+
+                if(square.getNumber() > 1 && numberBehind >= 1) {
+                    diagonals.add(new Square((char)letter, numberBehind));
+                    numberBehind--;
+                }
+            }
+        }
+
+        return diagonals;
+    }
+
+    private List<Square> leftDiagonal() {
+        List<Square> diagonals = new ArrayList<>();
+
+        if(square.getLetter() != 'A') {
+            int numberAhead = square.getNumber() + 1;
+            int numberBehind = square.getNumber() - 1;
+
+            for (int letter = ((int)square.getLetter() - 1); letter >= 65; letter--) {
+                if(square.getNumber() < 8 && numberAhead <= 8) {
+                    diagonals.add(new Square((char)letter, numberAhead));
+                    numberAhead++;
+                }
+
+                if(square.getNumber() > 1 && numberBehind >= 1) {
+                    diagonals.add(new Square((char)letter, numberBehind));
+                    numberBehind--;
+                }
+            }
+        }
+
+        return diagonals;
+    }
+}
